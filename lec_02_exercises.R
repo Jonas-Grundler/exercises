6a0a4ec0c9f11437b30cff9268a7361a9fc5e45a
Jonas-Grundler <s2065226@ed.ac.uk>
2020-10-05 06:36:12 +0000

    Worked on Ex2 and Ex4

--- lec_02_exercises.R
+++ lec_02_exercises.R
@@ -6,15 +6,19 @@
 ##
 
 c(1, NA+1L, "C")
+#character
 
 c(1L / 0, NA)
+#double
 
-c(1:3, 5)
+c(1:3,5)
+#double
 
 c(3L, NaN+1L)
+#double
 
 c(NA, TRUE)
-
+#logical
 
 ## Part 2
 ##
@@ -24,15 +28,17 @@ c(NA, TRUE)
 ## Hint - think about the pairwise interactions between types.
 ##
 
+#Answer: Character > Double > Integer > Logical
+
 
 
 
 # Exercise 2
 
 ## Below is a vector containing all prime numbers between 2 and 100:
-  
+
 primes = c( 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 
-           43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
 
 ## If you were given the vector x = c(3,4,12,19,23,51,61,63,78), write the R 
 ## code necessary to print only the values of x that are not prime 
@@ -42,3 +48,25 @@ primes = c( 2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
 ##
 
 x = c(3,4,12,19,23,51,61,63,78)
+i = 1L
+res = c()
+
+for(i in 1:length(x)) {
+  if (any(primes == x[i]))
+    res = c(res, x[i])
+}
+res
+
+
+
+
+z=1
+f = function(x,y,z){
+  z = x+y
+  g = function(m=x, n=y){
+    m/z + n/z
+  }
+  z*g()
+}
+f(1,2,x=3)
+#Answer: 4
