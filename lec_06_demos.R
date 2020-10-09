8f84c7f1c344312b804ad78f27df90115b6a2559
Colin Rundel <rundel@gmail.com>
2020-10-09 10:53:20 +0000

    Lec 6

---
+++ lec_06_demos.R
@@ -0,0 +1,11 @@
+# Demos
+
+## 1. How many flights to Los Angeles (LAX) did each of the legacy carriers 
+##    (AA, UA, DL or US) have in May from JFK, and what was their average duration?
+    
+
+
+## 2. What was the shortest flight out of each airport in terms of distance? 
+##    In terms of duration?
+
+
---
+++ lec_06_exercises.R
@@ -0,0 +1,11 @@
+# Exercise 1
+
+## 1. Which plane (check the tail number) flew out of each New York airport 
+##    the most?
+   
+
+ 
+## 2. Which date should you fly on if you want to have the lowest possible 
+##    average departure delay? What about arrival delay?
+
+
